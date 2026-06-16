fa4187b0528b7c062bcd02a7d4d423673a978a4c
nagammar994-prog <nagammar994@gmail.com>
2026-06-16 16:35:54 +0530

    Set up Express server with user endpoint
    Added a simple Express server with a user endpoint that returns a password.

--- sample-app/app.js
+++ sample-app/app.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const _ = require('lodash');
 
+const app = express();
+
+app.get('/user', (req, res) => {
+
+  const password = "admin123";
+
+  let data = null;
+
+  if (data == undefined) {
+    console.log("Data missing");
+  }
+
+  res.send(password);
+});
+
+app.listen(3000);
